b0de388fea47f5849620d3e0bd7efe00ce5c8cd9
samsonhumber <printerfissh693@gmail.com>
2022-09-22 16:20:25 +0100

    hopefully works for my path extensions now

--- server/app.py
+++ server/app.py
@@ -1,6 +1,11 @@
 import sys
-sys.path.append('/server/backend')
-import index 
+currentPath = sys.path[0]
+windows_extension = '\\'
+mac_extension = '/'
+sys.path.append(currentPath + windows_extension + 'backend')
+sys.path.append(currentPath + mac_extension + 'backend')
+print(sys.path) # Use this to check if your path is correctly processed
+
 from config import app
 
 if __name__ == '__main__':app.run(host="localhost", port=3000)
